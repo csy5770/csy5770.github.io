68aafecc9ce5d86eeda4bbcb7a28156cbd02f075
csy5770 <csy5770@test.com>
2021-05-14 15:42:07 +0900

    6교시 커밋

---
+++ test/json.js
@@ -0,0 +1,122 @@
+var jsonData = [
+    {
+      "_id": "609e1263405f782c9791657d",
+      "age": 25,
+      "eyeColor": "blue",
+      "name": "Lang Christian"
+    },
+    {
+      "_id": "609e126398bebf22b53e6716",
+      "age": 22,
+      "eyeColor": "green",
+      "name": "Berg Romero"
+    },
+    {
+      "_id": "609e1263b68e448543851146",
+      "age": 39,
+      "eyeColor": "blue",
+      "name": "Gould Saunders"
+    },
+    {
+      "_id": "609e1263e21926de4bb085e4",
+      "age": 36,
+      "eyeColor": "brown",
+      "name": "Gibbs Montgomery"
+    },
+    {
+      "_id": "609e126381c9409e872d57fa",
+      "age": 29,
+      "eyeColor": "blue",
+      "name": "Alissa Sloan"
+    },
+    {
+      "_id": "609e12639fbb2f0c5806a4e6",
+      "age": 31,
+      "eyeColor": "green",
+      "name": "Barnett Blair"
+    },
+    {
+      "_id": "609e1263b97a87f90be718c2",
+      "age": 27,
+      "eyeColor": "green",
+      "name": "Clayton Dotson"
+    },
+    {
+      "_id": "609e126389ddb83689fd0acb",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Dorothea Justice"
+    },
+    {
+      "_id": "609e12630ca65bd31fedb846",
+      "age": 25,
+      "eyeColor": "brown",
+      "name": "Lynn Mckinney"
+    },
+    {
+      "_id": "609e1263fb225663f8447c57",
+      "age": 39,
+      "eyeColor": "green",
+      "name": "Linda Mcknight"
+    },
+    {
+      "_id": "609e1263c4dcab2bbdd5f714",
+      "age": 27,
+      "eyeColor": "green",
+      "name": "Willis Mccall"
+    },
+    {
+      "_id": "609e1263da07d670737af3f4",
+      "age": 35,
+      "eyeColor": "brown",
+      "name": "Michael Nunez"
+    },
+    {
+      "_id": "609e1263289825fb7518121d",
+      "age": 27,
+      "eyeColor": "green",
+      "name": "Curry Lane"
+    },
+    {
+      "_id": "609e1263d9a769a0f6ae1de3",
+      "age": 23,
+      "eyeColor": "brown",
+      "name": "Regina Patton"
+    },
+    {
+      "_id": "609e126300af130bc0254195",
+      "age": 24,
+      "eyeColor": "green",
+      "name": "Katherine Melendez"
+    },
+    {
+      "_id": "609e1263bc8c1ae8e25a3f91",
+      "age": 30,
+      "eyeColor": "brown",
+      "name": "Mcbride Page"
+    },
+    {
+      "_id": "609e1263ae6747dd8a2fa7db",
+      "age": 26,
+      "eyeColor": "blue",
+      "name": "Anna Ware"
+    },
+    {
+      "_id": "609e1263ab383306276fd000",
+      "age": 25,
+      "eyeColor": "brown",
+      "name": "Jarvis Hardin"
+    },
+    {
+      "_id": "609e1263593946687572336f",
+      "age": 35,
+      "eyeColor": "brown",
+      "name": "Kerr Head"
+    },
+    {
+      "_id": "609e12630bbbaa7ed782f6ce",
+      "age": 27,
+      "eyeColor": "green",
+      "name": "Lucia Bryant"
+    }
+  ]
